26abd2cfc7b14ed59d285312fe9b4ff5a33e0425
Jhon Mike <developer@jhonmike.com.br>
2015-10-27 02:17:15 -0200

    skeleton apigility

---
+++ config/autoload/global.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'oauth' => array(
+                'options' => array(
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ),
+                'type' => 'regex',
+            ),
+        ),
+    ),
+    'db' => array(
+        'adapters' => array(
+            'Db\\Adapter' => array(),
+        ),
+    ),
+    'zf-mvc-auth' => array(
+        'authentication' => array(
+            'map' => array(
+                'ApiAuth\\V1' => 'adapterauth',
+                'Topografia\\V1' => 'adapterauth',
+            ),
+        ),
+    ),
+);
